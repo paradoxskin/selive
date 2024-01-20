c1c7df5c64b1d0a4ecbb2ec94173033701262a8d
paradoxskin <1312269430@qq.com>
2024-01-20 11:37:35 +0800

    change config format, use direct geckodriver path

--- se.py
+++ se.py
@@ -1,29 +1,26 @@
-from configparser import ConfigParser
 from selenium import webdriver
 from time import sleep
+import config
 
 def start_se(echo, shutdown):
-    config = ConfigParser()
-    config.read("config.ini")
-
-    profile = config.get("se", "profile_dir_path")
-    url = config.get("se", "live_url")
-
-    ser = webdriver.firefox.service.Service(log_output="./log/se.log")
-
+    ser = webdriver.firefox.service.Service(log_output="./log/se.log", executable_path=config.driver_path)
     ops = webdriver.firefox.options.Options()
     ops.add_argument("-profile")
-    ops.add_argument(profile)
+    ops.add_argument(config.profile_dir_path)
     ops.add_argument("--headless")
 
     ff = webdriver.Firefox(service=ser, options=ops)
-    ff.get(url)
+    ff.get(config.live_url)
     echo("[+] se ok.")
     echo('[i] press "q" to quit')
 
     sleep(5)
-    ff.find_element("tag name", "video").click()
-    ff.find_element("class name", "left-area").find_element("tag name", "span").click()
+    try:
+        ff.find_element("tag name", "video").click()
+        ff.find_element("class name", "left-area").find_element("tag name", "span").click()
+        echo("[i] player stop.")
+    except:
+        echo("[!] player cant stop.")
 
     with shutdown:
         shutdown.wait()
